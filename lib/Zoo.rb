4108b5045616691ea208f3eed9f4793717b8f9cf
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-25 17:25:24 -0500

    commit tree 9c52868b295e09aaf4be01a1d63cd86b79a179ae

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
+
 class Zoo
 
+@@all = []
+
+attr_reader :name, :location  
+
+    def initialize(name, location)
+        @location = location
+        @name = name
+        @@all << self    
+    end
+
+    def self.location
+        @location
+    end
+
+    def self.name
+        @name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animals.all.select{|animal| animal.zoo == self}
+    end
 end
+
--- run.rb
+++ run.rb
@@ -2,7 +2,7 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
-
+zoo1 = Zoo.new("tiger", "nyc")
 #Test your code here
 
 
